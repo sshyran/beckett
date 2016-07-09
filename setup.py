a450d5add373ae92441f392de1c571315f865616
Paul Hallett <paul.hallett@lyst.com>
2016-07-09 11:59:24 +0100

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='beckett',
-    version='0.7.0',
+    version='0.7.1',
     description="Hypermedia API Client Framework",
     long_description=readme,
     author="Paul Hallett",
